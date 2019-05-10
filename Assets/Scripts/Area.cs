80999ff1fff90c9a4358b62b804802d8a1a51abd
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-05-10 18:15:25 +0200

    Początek

---
+++ Assets/Scripts/Area.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour
+{
+
+    public static Area instance;
+
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+
+    void Start()
+    {
+        
+    }
+
+    void Awake() {
+        instance = this;
+
+        float screenHeight = Screen.height / 2;
+        float screenWidth = Screen.width / 2;
+
+        screenTop = screenHeight;
+        screenBottom = -screenHeight;
+        screenRight = screenWidth;
+        screenLeft = -screenWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        other.GetComponent<Object>().SetTeleportation(true);
+        Debug.Log("Coś koliduje");
+    }
+
+    public void OnTriggerExit2D(Collider2D other) {
+        other.GetComponent<Object>().SetTeleportation(true);
+    }
+}
---
+++ Assets/Scripts/TestObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObj : Object
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Input.GetAxis("Horizontal");
+
+        if(Input.GetAxis("Vertical") > 0) {
+            Move();
+        } 
+
+        Rotation(angle);
+        if(isTeleporting){
+            Teleportation();
+        }
+        
+    }
+}

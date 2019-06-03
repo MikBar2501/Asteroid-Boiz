0e5d434fd58fadb894fa5ea09b2caf73c8beb5bc
Emejpi <emejpi@gmail.com>
2019-06-03 11:53:43 +0200

    lvl obj

--- Assets/Scripts/LevelObject.cs
+++ Assets/Scripts/LevelObject.cs
@@ -2,21 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelObject : MovableObject
+public class LevelObject : MonoBehaviour
 {
-    public override void Initialize()
+     void Start()
     {
-        base.Initialize();
         GameManager.instance.AddLevelObject(gameObject);
-        //directionVector = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
-    }
-
-    protected override void ImplementCollisions()
-    {
-        base.ImplementCollisions();
-
-        collisionActions.Add(ObjType.Bullet, new Action.ActionDestroy());
-        collisionActions.Add(ObjType.Player, new Action.ActionDestroy());
     }
 
     private void OnDestroy()
